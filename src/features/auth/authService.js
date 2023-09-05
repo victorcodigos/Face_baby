c55b70731f360b32018ae7b4e094e4dd59364490
apl09 <adrianpastorlopez09@gmail.com>
2023-09-05 15:29:45 +0200

    seems alraight

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000";
 const register = async (userData) => {
-    const res = await axios.post(API_URL + "/users", userData);
+    const res = await axios.post(API_URL + "/users/register", userData);
     return res.data;
 };
 

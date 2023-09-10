385cab31abbbcd122623a041256b07de0fab2947
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-09-10 02:36:25 +0200

    Import users in the app folder (store.js)

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import auth from '../features/auth/authSlice'
 import posts from '../features/posts/postsSlice'
+import users from '../features/users/usersSlice'
 
 export const store = configureStore({
 
     reducer: {
 
         auth,
-        posts
+        posts,
+        users
     },
 });
